777d7e5c1437f6ff65083979ba376cdf116ec426
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-04-26 03:05:34 -0700

    Installed Axios package and added API call to fetch dictionary words

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function DictionarySearch() {
   const [searchInput, setSearchInput] = useState("");
 
+  function fetchWord(response) {
+    console.log(response);
+  }
+
   function handleSearch(event) {
     event.preventDefault();
-    alert(searchInput);
+    let apiKey = "7f30420fc505ct92a4f1o960ab77843b";
+    let api = `https://api.shecodes.io/dictionary/v1/define?word=${searchInput}&key=${apiKey}`;
+    axios.get(api).then(fetchWord);
   }
 
   function handleInputChange(event) {
